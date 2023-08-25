205e681de8a93d4d9f54b029a5cbc113345efca1
GiovA07 <76705663+GiovA07@users.noreply.github.com>
2023-08-25 19:10:16 -0300

    Revert "Revert "Paso 2. Ref. #1 revisado""

--- hello.py
+++ hello.py
@@ -1,5 +1,15 @@
-from flask import Flask
+from flask import Flask, render_template
+
 app = Flask(__name__)
+
 @app.route("/")
-def hello_world():
-    return "<p>Hello, World!</p>" 
+def principal():
+    return render_template('index.html')
+
+# @app.route("/login")
+# def login():
+#     return "hola mundo"
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
